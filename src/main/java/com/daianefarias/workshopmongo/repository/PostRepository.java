6666649a80760a70eb74bb64cc70b86e54f41ca1
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-09-02 22:16:38 +0100

    Consulta simples com query methods

--- src/main/java/com/daianefarias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/daianefarias/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.daianefarias.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.daianefarias.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContaining(String text);
 
 }
---
+++ src/main/java/com/daianefarias/workshopmongo/resources/util/URL.java
@@ -0,0 +1,16 @@
+package com.daianefarias.workshopmongo.resources.util;
+
+import java.net.URLDecoder;
+
+public class URL {
+	
+	public static String decodeParam(String text) {
+	    try {
+	    	return URLDecoder.decode(text, "UTF-8");
+		} catch (Exception e) {
+			return "";
+		}
+		
+	}
+
+}
