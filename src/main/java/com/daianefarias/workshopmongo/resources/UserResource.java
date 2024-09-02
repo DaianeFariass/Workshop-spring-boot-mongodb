1a1c4da6706d74ccfc8fc318b061823dd693764f
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-09-02 18:35:25 +0100

    Atualização de usuário com PUT

--- src/main/java/com/daianefarias/workshopmongo/resources/UserResource.java
+++ src/main/java/com/daianefarias/workshopmongo/resources/UserResource.java
@@ -48,10 +48,19 @@ public class UserResource {
         return ResponseEntity.created(uri).build();				
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value = "/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody User obj, @PathVariable String id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 		
 }
