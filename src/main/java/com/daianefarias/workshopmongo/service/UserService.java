acd30b7a612063da742442c1328e3603d886bf9a
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-09-02 16:35:35 +0100

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/daianefarias/workshopmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.daianefarias.workshopmongo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.daianefarias.workshopmongo.domain.User;
+import com.daianefarias.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
