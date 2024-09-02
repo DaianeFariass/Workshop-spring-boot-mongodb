84b6de3622a72399b628c60378b9163ebbb47223
Daiane Farias <daiane.farias.contractor@gmail.com>
2024-09-02 23:07:37 +0100

    Consulta com vários critérios

--- src/main/java/com/daianefarias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/daianefarias/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.daianefarias.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,7 +8,6 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.daianefarias.workshopmongo.domain.Post;
-import com.daianefarias.workshopmongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -16,5 +16,13 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , "
+			+ "{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'body': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
+	
 
 }
